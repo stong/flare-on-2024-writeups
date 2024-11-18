8808a37bb43cf81de53d09e59cb2656173a3a450
cts <14918218+stong@users.noreply.github.com>
2024-11-18 02:51:17 -0500

    Update Program.cs

--- 7-fullspeed/ConsoleApp1/ConsoleApp1/Program.cs
+++ 7-fullspeed/ConsoleApp1/ConsoleApp1/Program.cs
@@ -163,7 +163,7 @@ namespace ConsoleApp1
 
         static void DoSolve()
         {
-                        var q = new Org.BouncyCastle.Math.BigInteger("c90102faa48f18b5eac1f76bb40a1b9fb0d841712bbe3e5576a7a56976c2baeca47809765283aa078583e1e65172a3fd", 16);
+            var q = new Org.BouncyCastle.Math.BigInteger("c90102faa48f18b5eac1f76bb40a1b9fb0d841712bbe3e5576a7a56976c2baeca47809765283aa078583e1e65172a3fd", 16);
             var a = new Org.BouncyCastle.Math.BigInteger("a079db08ea2470350c182487b50f7707dd46a58a1d160ff79297dcc9bfad6cfc96a81c4a97564118a40331fe0fc1327f", 16);
             var b = new Org.BouncyCastle.Math.BigInteger("9f939c02a7bd7fc263a4cce416f4c575f28d0c1315c4f0c282fca6709a5f9f7f9c251c9eede9eb1baa31602167fa5380", 16);
             var gx = new Org.BouncyCastle.Math.BigInteger("087b5fe3ae6dcfb0e074b40f6208c8f6de4f4f0679d6933796d3b9bd659704fb85452f041fff14cf0e9aa7e45544f9d8", 16);
